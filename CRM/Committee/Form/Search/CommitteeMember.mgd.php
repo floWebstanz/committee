6725187de98624d0187adb5fb117ccf90858e5d0
Xavier Dutoit <xavier@tttp.eu>
2013-12-13 09:49:30 +0100

    initial push

---
+++ CRM/Committee/Form/Search/CommitteeMember.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Committee_Form_Search_CommitteeMember',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'CommitteeMember',
+      'description' => 'CommitteeMember (eu.tttp.committee)',
+      'class_name' => 'CRM_Committee_Form_Search_CommitteeMember',
+    ),
+  ),
+);
